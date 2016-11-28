80f9e04cc7d3ac30b36c5c7b64e3fef9333e6bb5
ashlinaronin <ashlinaronin@gmail.com>
2016-11-27 17:50:21 -0800

    Use port 2000 by default

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var cfg = {};
 
 // HTTP Port to run our web application
-cfg.port = process.env.PORT || 3000;
+cfg.port = process.env.PORT || 2000;
 
 // Export configuration object
 module.exports = cfg;
