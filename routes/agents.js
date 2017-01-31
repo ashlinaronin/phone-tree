46a1094a4d72e9325636299c72331994adb8f43e
ashlinaronin <ashlinaronin@gmail.com>
2017-01-31 00:36:04 -0800

    Fix agent routes

--- routes/agents.js
+++ routes/agents.js
@@ -1,11 +1,13 @@
 const cointreau = require('./agents/cointreau');
 const jenavieve = require('./agents/jenavieve');
 const ricardo = require('./agents/ricardo');
+const ada = require('./agents/ada');
 
 let agents = require('express').Router();
 
 agents.use('/cointreau', cointreau);
 agents.use('/jenavieve', jenavieve);
 agents.use('/ricardo', ricardo);
+agents.use('/ada', ada);
 
 module.exports = agents;
