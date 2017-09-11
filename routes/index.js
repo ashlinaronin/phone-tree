6df53d192de6e4b2e9ccf17af442852b75cf8a2a
ashlinaronin <ashlinaronin@gmail.com>
2017-09-11 09:51:25 -0700

    Add error handling at /error

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,22 @@ router.post('/', twilio.webhook({ validate: false }), (req, res) => {
     res.send(twiml);
 });
 
+router.post('/error', twilio.webhook({ validate: false}), (req, res) => {
+    let twiml = new twilio.TwimlResponse();
+
+    twiml.say(`We're sorry, we're currently experiencing technical difficulties. Please hold
+        while I transfer you to the next available representative`, SALES_VOICE);
+
+    let selectedAgent = agent.randomAgent();
+    let agentExtension = extensions.getDepartmentExtension(selectedAgent);
+
+    twiml.play({ digits: agentExtension });
+
+    twiml.redirect(`${baseUrl}/agents/${selectedAgent}`);
+
+    res.send(twiml);
+});
+
 router.use('/dingus', dingus);
 router.use('/agents', agents);
 router.use('/products', products);
